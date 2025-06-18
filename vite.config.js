0f0aacbeca9be465fab40f965cd262015b90a318
skarb-by <skarb_gaspadar@mail.ru>
2025-06-18 11:40:34 +0300

    8

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/CarsSlider/', // ← ОБЯЗАТЕЛЬНО для GitHub Pages!
+  base: '/CarsSlider/', // Это ОБЯЗАТЕЛЬНО для GitHub Pages
   plugins: [react()],
 })
